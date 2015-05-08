b5274cbf0fa872c5ace2f0905e7e210bd291141d
Gary Anderson <ganderson@seattleacademy.org>
2015-05-08 08:09:37 -0700

    fix lookup urban dictionary

--- urbandictionary/app.js
+++ urbandictionary/app.js
@@ -1,7 +1,45 @@
-
-
 var https = require('https');
 var fs = require('fs');
+
+var select = require('soupselect').select;
+var htmlparser = require("htmlparser");
+var http = require('http');
+var sys = require('sys');
+
+// var host = 'www.urbandictionary.com';
+// var client = http.createClient(80, host);
+// var request = client.request('GET', '/define.php?term=Asymptote', {
+//     'host': host
+// });
+
+// request.on('response', function(response) {
+//     response.setEncoding('utf8');
+
+//     var body = "";
+//     response.on('data', function(chunk) {
+//         body = body + chunk;
+//     });
+
+//     response.on('end', function() {
+
+//         // now we have the whole body, parse it and select the nodes we want...
+//         var handler = new htmlparser.DefaultHandler(function(err, dom) {
+//             if (err) {
+//                 console.log("Error: " + err);
+//             } else {
+//                 //console.log('dom',dom)
+//                 var meaning = select(dom, '.meaning');
+//                 console.log('meaning', meaning[0].children[0].data);
+
+//             }
+//         });
+
+//         var parser = new htmlparser.Parser(handler);
+//         parser.parseComplete(body);
+//     });
+// });
+// request.end();
+
 var options = {
     key: fs.readFileSync('/etc/ssl/server.key'),
     cert: fs.readFileSync('/etc/ssl/server.crt'),
@@ -13,14 +51,61 @@ https.createServer(options, function(req, res) {
 
     function sendResponse() {
         myResponse = JSON.stringify(echoResponse);
-        console.log(myResponse, {depth:5});
+        console.log(myResponse, {
+            depth: 5
+        });
         res.setHeader('Content-Length', myResponse.length);
         res.writeHead(200);
         res.end(myResponse);
     }
 
+    function getDefinition(word) {
+
+        var host = 'www.urbandictionary.com';
+        var client = http.createClient(80, host);
+        word = word.charAt(0).toUpperCase() + word.slice(1);
+        console.log('word',word);
+        var request = client.request('GET', '/define.php?term='+word, {
+            'host': host
+        });
+        request.on('response', function(response) {
+            response.setEncoding('utf8');
+
+            var body = "";
+            response.on('data', function(chunk) {
+                body = body + chunk;
+            });
+
+            response.on('end', function() {
+
+                // now we have the whole body, parse it and select the nodes we want...
+                var handler = new htmlparser.DefaultHandler(function(err, dom) {
+                    if (err) {
+                        console.log("Error: " + err);
+                    } else {
+                        //console.log('dom',dom)
+                        var meaning = select(dom, '.meaning');
+                        //console.log('meaning', meaning[0].children[0].data);
+                        if (meaning){
+                        meaning = meaning[0].children[0].data;
+                    }
+                    else meaning = "The definition for " + word + " is not defined";
+                        meaning = meaning.replace(/\W/g, ' ');
+                        console.log(meaning);
+                        echoResponse.response.outputSpeech.text = meaning;
+                        echoResponse.response.card.content = meaning;
+                        sendResponse();
+
+                    }
+                });
+                var parser = new htmlparser.Parser(handler);
+                parser.parseComplete(body);
+            });
+        });
+        request.end();
+    }
+
     if (req.method == 'POST') {
-        console.log('post called')
         var jsonString = '';
         req.on('data', function(data) {
             jsonString += data;
@@ -35,10 +120,12 @@ https.createServer(options, function(req, res) {
             echoResponse.response.outputSpeech.text = "ok"
             echoResponse.response.shouldEndSession = "false";
             theRequest = JSON.parse(jsonString);
-            console.dir(theRequest, {depth: 5});
+            // console.dir(theRequest, {
+            //     depth: 5
+            // });
             if (theRequest.request.type == 'IntentRequest') {
                 word = theRequest.request.intent.slots.Word.value;
-                console.log("word", word);
+                //console.log("word", word);
                 echoResponse.response.card = {};
                 echoResponse.response.card.type = "Simple";
                 echoResponse.response.card.title = "Urban Dictionary";
@@ -46,15 +133,7 @@ https.createServer(options, function(req, res) {
                 echoResponse.response.card.content = "";
                 echoResponse.sessionAttributes = {};
                 echoResponse.response.shouldEndSession = "false";
-                //EnBible.getVerse(verseString, function(err, data) {
-                    //console.log(err || data[0].text);
-                    text = "here is the definition";
-                    text = text.replace(/\W/g, ' '); //remove special characters
-                    echoResponse.response.outputSpeech.text = text;
-                    echoResponse.response.card.content = text;
-                    sendResponse();
-
-                //});
+                getDefinition(word);
             } else
                 sendResponse();
         });
