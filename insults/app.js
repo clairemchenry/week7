d2de1ccd27ae60e78136427646aaa905386995c4
cm <clairemchenry@seattleacademy.org>
2015-05-11 13:43:57 -0700

    work on insults

---
+++ insults/app.js
@@ -0,0 +1,80 @@
+var https = require('https');
+var fs = require('fs');
+var options = {
+    key: fs.readFileSync('/etc/ssl/server.key'),
+    cert: fs.readFileSync('/etc/ssl/server.crt'),
+    ca: fs.readFileSync('/etc/ssl/server.ca.crt')
+};
+
+var truths = [];
+truths.push("You have very smooth hair.");
+truths.push("You are quite strapping.");
+truths.push("I really like your socks");
+truths.push("If I freeze, it is not a bug, I was stunned by your beauty.")
+truths.push("The sound of your voice sends tingles of joy down my back.")
+truths.push("I would really enjoy a road trip with you!")
+
+
+
+function getRandomTruth() {
+    return truths[Math.floor(Math.random() * truths.length)];
+}
+
+
+
+
+https.createServer(options, function(req, res) {
+    if (req.method == 'POST') {
+        var jsonString = '';
+        req.on('data', function(data) {
+            jsonString += data;
+        });
+        req.on('end', function() {
+            console.dir(jsonString, {
+                depth: 5
+            });
+            echoResponse = {};
+            echoResponse.version = "1.0";
+            echoResponse.response = {};
+            echoResponse.response.outputSpeech = {};
+
+
+            echoResponse.response.outputSpeech.type = "PlainText"
+        
+            echoResponse.response.shouldEndSession = "false";
+            theRequest = JSON.parse(jsonString);
+            console.log('JSON', theRequest.request);
+            truth = getRandomTruth();
+                   
+                    echoResponse.response.outputSpeech.text = truth;
+            if (typeof theRequest.request.intent !== 'undefined') {
+                choice = theRequest.request.intent.slots.Choice.value;
+                
+                
+                    //truth = getRandomTruth();
+                   
+                    echoResponse.response.outputSpeech.text = choice + " " + truth; 
+                    //echoResponse.response.outputSpeech.text = "you said " + choice;
+                    // echoResponse.response.card = {}
+                    // echoResponse.response.card.type = "PlainText";
+                    // echoResponse.response.card.title = choice;
+                    // echoResponse.response.card.subtitle = choice;
+                    // echoResponse.response.card.content = choice;
+                    echoResponse.response.shouldEndSession = "true";
+            
+      
+            }
+            myResponse = JSON.stringify(echoResponse);
+            res.setHeader('Content-Length', myResponse.length);
+            res.writeHead(200);
+            res.end(myResponse);
+            console.log('from post', myResponse);
+
+        });
+    } else {
+        myResponse = JSON.stringify(echoResponse);
+        res.setHeader('Content-Length', myResponse.length);
+        res.writeHead(200);
+        res.end(myResponse);
+    }
+}).listen(3017); //Put number in the 3000 range for testing and 443 for production
